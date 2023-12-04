455297ff4fc7be6b76493485ad54c84ed72fd48c
rowend36 <rowendduke36@gmail.com>
2023-12-04 22:18:00 +0100

    feat: better build ace

--- scripts/build_ace.sh
+++ scripts/build_ace.sh
@@ -1,5 +1,6 @@
 DIR=`dirname $0`
 DIR=$DIR/../libs
 node $DIR/ace/Makefile.dryice.js $@ -nc
+mkdir -p $DIR/../src/client/libs/ace/
 cp -r $DIR/ace/build/src-noconflict/* $DIR/../src/client/libs/ace/
 
